ba1c727f02af0c8436a424328d04d18d462be00d
Logan Cox <mail@logan-cox.com>
2023-04-25 21:02:47 +0100

    adding opperator function to take in two numbers and an action then return a result

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let numOne;
+let numTwo;
+let action;
+
 function add (numOne, numTwo) {
     return numOne + numTwo;
 }
@@ -12,4 +16,20 @@ function multiply (numOne, numTwo) {
 
 function divide (numOne, numTwo) {
     return numOne / numTwo;
+}
+
+function opperation (numOne, numTwo, action) {
+    switch(action) {
+        case "add":
+            return add(numOne, numTwo);
+
+        case "subtract":
+            return subtract(numOne, numTwo);
+
+        case "multiply":
+            return multiply(numOne, numTwo);
+
+        case "divide":
+            return divide(numOne, numTwo);
+    }
 }
