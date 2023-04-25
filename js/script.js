f6e6be7afebe77603ba26f0c7dc73c38d8360700
Logan Cox <mail@logan-cox.com>
2023-04-25 20:41:28 +0100

    adding functionality to add, devide, subtract and multiply

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function add (numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtract (numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply (numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide (numOne, numTwo) {
+    return numOne / numTwo;
+}
